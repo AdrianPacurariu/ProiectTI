15bc7b683716c85173db46ead107cbce35336328
Păcurariu Rareș-Adrian <rares.pacurariu@student.upt.ro>
2023-11-19 23:35:23 +0200

    added procente page, password protected and with change password feature (password is encrypted and inserted/retrieved from the database)

--- PaymentWebApp/Procente.aspx
+++ PaymentWebApp/Procente.aspx
@@ -1,9 +1,78 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/HomePage.Master" AutoEventWireup="true" CodeBehind="Procente.aspx.cs" Inherits="PaymentWebApp.Procente" %>
 
 <asp:Content ID="Procente" ContentPlaceHolderID="MainContent" runat="server">
-     <div class="container">
-        <div class="text-align: left mt-5">
-            <h4>Procente</h4>
+    <div class="container">
+        <div class="container text-center">
+            <div class="mt-5">
+                <h2>Modificare procente salarizare</h2>
+            </div>
         </div>
+        <br />
+        <br />
+        <center>
+            <asp:Label ID="InfoLabel" runat="server" Text="Zonă protejată: Pentru a modifica valorile procentuale ale grilei de salarizare, introduceți parola."></asp:Label>
+            <br />
+            <asp:Panel ID="LoginPanel" runat="server">
+                <asp:TextBox ID="PasswordTextbox" runat="server" TextMode="Password"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button ID="LoginButton" runat="server" Text="Autentificare" OnClick="LoginButton_Click" />
+                <br />
+                <br />
+                <asp:Label ID="ErrorLabel" runat="server" ForeColor="Red" Visible="false"></asp:Label>
+
+            </asp:Panel>
+            <br />
+            <br />
+
+            <asp:Panel ID="DataPanel" runat="server" Visible="false">
+                <asp:GridView ID="ProcenteGridView" runat="server" AutoGenerateColumns="False" CssClass="table table-bordered">
+                    <Columns>
+                        <asp:BoundField DataField="cas_procent" HeaderText="CAS (%)" />
+                        <asp:BoundField DataField="cass_procent" HeaderText="CASS (%)" />
+                        <asp:BoundField DataField="impozit_procent" HeaderText="Impozit (%)" />
+                    </Columns>
+                    <RowStyle CssClass="gridview-row" />
+                </asp:GridView>
+                <br />
+                <asp:Button ID="BackButton" runat="server" Text="Înapoi" OnClick="BackButton_Click" />
+                <asp:Button ID="ChangePasswordButton" runat="server" Text="Schimbare parolă" OnClientClick="changePassword();" />
+
+            </asp:Panel>
+        </center>
     </div>
+
+   <script type="text/javascript">
+       function changePassword() {
+           var newPassword = prompt("Introduceți noua parolă:", "");
+           if (newPassword != null && newPassword != "") {
+               var confirmPassword = prompt("Confirmați noua parolă:", "");
+               if (confirmPassword != null && confirmPassword != "") {
+                   if (newPassword === confirmPassword) {
+                       $.ajax({
+                           type: "POST",
+                           url: "Procente.aspx/ChangePassword",
+                           data: JSON.stringify({ newPassword: newPassword }),
+                           contentType: "application/json; charset=utf-8",
+                           dataType: "json",
+                           success: function (response) {
+                               alert(response.d);
+                           },
+                           error: function (error) {
+                               alert("A apărut o eroare în timpul schimbării parolei: " + error.responseText);
+                           }
+                       });
+                   } else {
+                       alert("Parolele nu se potrivesc. Vă rugăm să încercați din nou.");
+                   }
+               } else {
+                   alert("Parola nu a fost confirmată.");
+               }
+           } else {
+               alert("Parola nu a fost introdusă.");
+           }
+       }
+   </script>
+
+
 </asp:Content>
--- PaymentWebApp/Procente.aspx.cs
+++ PaymentWebApp/Procente.aspx.cs
@@ -1,17 +1,164 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Web.UI;
+using System.Web;
 
 namespace PaymentWebApp
 {
     public partial class Procente : System.Web.UI.Page
     {
+        private string connectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                InfoLabel.Visible = true;
+                LoginPanel.Visible = true;
+                DataPanel.Visible = false;
+                ErrorLabel.Visible = false;
+            }
+            else
+            {
+                if (Request.Form["__EVENTTARGET"] == "BackButton")
+                {
+                    InfoLabel.Visible = true;
+                    LoginPanel.Visible = true;
+                    DataPanel.Visible = false;
+                    ErrorLabel.Visible = false;
+                }
+            }
         }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            string enteredPassword = PasswordTextbox.Text;
+            string storedPassword = RetrievePassword();
+
+            if (storedPassword != null && VerifyPassword(enteredPassword, storedPassword))
+            {
+                InfoLabel.Visible = false;
+                LoginPanel.Visible = false;
+                DataPanel.Visible = true;
+                DisplayProcenteData();
+            }
+            else
+            {
+                ErrorLabel.Visible = true;
+                ErrorLabel.Text = "Parolă incorectă. Încercați din nou.";
+                ErrorLabel.ForeColor = Color.Red;
+            }
+        }
+
+        protected void BackButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(Request.RawUrl);
+        }
+
+        private string RetrievePassword()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string selectQuery = "SELECT password FROM security";
+                MySqlCommand command = new MySqlCommand(selectQuery, connection);
+
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    return (result != null) ? result.ToString() : null;
+                }
+                catch (Exception ex)
+                {
+                    string alertScript = "<script>alert('A intervenit o eroare!');</script>";
+                    Response.Write(alertScript);
+                    return null;
+                }
+            }
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedHashedPassword)
+        {
+            return (ComputeSha256Hash(enteredPassword) == storedHashedPassword);
+        }
+
+        private static string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void DisplayProcenteData()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string selectQuery = "SELECT cas_procent, cass_procent, impozit_procent FROM procente";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, connection);
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    adapter.Fill(dataTable);
+
+                    ProcenteGridView.DataSource = dataTable;
+                    ProcenteGridView.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    string alertScript = "<script>alert('A intervenit o eroare!');</script>";
+                    Response.Write(alertScript);
+                }
+            }
+        }
+
+        [System.Web.Services.WebMethod]
+        public static string ChangePassword(string newPassword)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+
+            try
+            {
+                string encryptedPassword = ComputeSha256Hash(newPassword);
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string updateQuery = "UPDATE security SET password = @password";
+                    MySqlCommand command = new MySqlCommand(updateQuery, connection);
+                    command.Parameters.AddWithValue("@password", encryptedPassword);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        return "Parola a fost schimbată cu succes!";
+                    }
+                    else
+                    {
+                        return "A intervenit o eroare la schimbarea parolei!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "A intervenit o eroare!";
+            }
+        }
+
     }
 }
